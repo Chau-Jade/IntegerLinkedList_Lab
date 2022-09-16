e5b12324ecdbbe6b317c2c8f8c778415cb008546
Chau-Jade <chaunm24t1@gmail.com>
2022-09-16 22:52:22 +1200

    Added the addFront and the add method to the IntegerLinkedList class.

--- IntegerLinkedList.cpp
+++ IntegerLinkedList.cpp
@@ -18,20 +18,46 @@ IntegerLinkedList::IntegerLinkedList() {
 
 void IntegerLinkedList::print() const {
 	// Set pointer to print to be the head pointer
-	Node *nodeToPrint = this->head;
+	Node *nodePointer = this->head;
 
 	// Print out the first node
-	cout << nodeToPrint->getValue();
+	cout << nodePointer->getValue();
 
 	// Loop through the linked list, to get the address of adjacent nodes
-	while (nodeToPrint->getNext() != 0) {
-		cout << " -> " << nodeToPrint->getNext()->getValue();
-		nodeToPrint = nodeToPrint->getNext();
+	while (nodePointer->getNext() != 0) {
+		cout << " -> " << nodePointer->getNext()->getValue();
+		nodePointer = nodePointer->getNext();
 	}
 
 	cout << endl;
 }
 
+void IntegerLinkedList::add(int v) {
+        // If linked list is empty, add the new node to the linked list 
+        if (this->head == 0) {
+                this->head = new Node(v);
+        } else {
+                nodePointer = this->head;
+                while (nodePointer->getNext() != 0) { // move to the last node of the linked list. 
+                       nodePointer = nodePointer->getNext();
+                }
+                node->setNext(new Node(v));
+        }
+}
+
+void IntegerLinkedList::addFront(int v) {
+        // Create a new node on the heap
+        Node *n = new Node(v);
+        
+        // Move the current head to be the second element in the linked list
+        n->setNext(this->head);
+        
+        // Set the head to be the newly created node.
+        this->head = n;
+        
+        // Increase the total number of nodes in the linked list by 1
+        this->countNodes++;
+}
 IntegerLinkedList::~IntegerLinkedList() {
 	// TODO Auto-generated destructor stub
 }
